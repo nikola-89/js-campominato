7b6fd6410f9cfa80657eb7d7567026ecd799ae0a
Nicola <nicola.ba1989@gmail.com>
2020-01-14 17:56:38 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var cpuNumber = [];
+for (var i = 0; i < 16; i++) {
+    cpuNumber.push(getRandomIntInclusive(1, 100))
+}
+console.log('CPU: ' + cpuNumber);
+
+var counter = 1;
+var userNumberArray = [];
+var userNumber = parseInt(prompt('[' + counter + '] Inserisci un numero:'))
+
+while (counter !== 85 && checkNumberInArray(cpuNumber, userNumber) == false) {
+    console.log('Contatore: ' + counter);
+    console.log('USER: ' + userNumberArray);
+    userNumber = parseInt(prompt('[' + counter + '] Inserisci un numero:'))
+    userNumberArray.push(userNumber);
+    if (counter === 84) {
+        alert('Hai vinto! I tuoi numeri consentiti: ' + userNumberArray)
+    }
+    counter += 1;
+}
+
+if (checkNumberInArray(cpuNumber, userNumber)) {
+    alert('Hai perso! Il tuo numero: ' + userNumber + ' | Numeri CPU: ' + cpuNumber);
+}
+
+// **************************************************************************
+function checkNumberInArray(array, number) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === number) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
