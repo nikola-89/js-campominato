6ef2fc9fdb028ecc340266dc0b6e3ecf35da70d0
Nicola <nicola.ba1989@gmail.com>
2020-01-15 17:37:01 +0100

    tolto ultimo controllo 84 che è già valutato dal do while quindi inutile

--- js/script.js
+++ js/script.js
@@ -6,22 +6,23 @@ while (cpuNumberArray.length < 16) {
     }
 }
 
-var counter = 1;
 var userNumberArray = [];
-var userNumber;
+var counter = 1;
 
+// do_while - senza messaggi di errore
+var userNumber;
 do {
     userNumber = parseInt(prompt('[' + counter + '] Inserisci un numero da 1 a 100:'));
     if (checkNumberInArray(userNumberArray, userNumber) == false && checkIs1to100(userNumber) && checkNumberInArray(cpuNumberArray, userNumber) == false && isNaN(userNumber) == false) {
         userNumberArray.push(userNumber);
         counter++;
     }
-} while (checkNumberInArray(cpuNumberArray, userNumber) == false && userNumberArray.length != 84);
+} while (checkNumberInArray(cpuNumberArray, userNumber) == false && userNumberArray.length != 3);
 
 if (checkNumberInArray(cpuNumberArray, userNumber)) {
     alert('Hai perso!\nHai inserito: ' + userNumber + '\nNumeri CPU: ' + cpuNumberArray + '\nI tuoi numeri: ' + userNumberArray + '\nPunteggio: ' + userNumberArray.length);
 }
-else if (userNumberArray.length == 84) {
+else {
     alert('Hai vinto!\nNumeri CPU: ' + cpuNumberArray + '\nI tuoi numeri: ' + userNumberArray + '\nPunteggio: ' + userNumberArray.length);
 }
 
