bb2b305103f893f63914dd93c33dfae54bf1c2c5
Nicola <nicola.ba1989@gmail.com>
2020-01-15 09:58:46 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var cpuNumber = [];
 for (var i = 0; i < 16; i++) {
-    cpuNumber.push(getRandomIntInclusive(1, 100))
+    var cpuRandomNumber = getRandomIntInclusive(1, 100);
+    if (checkNumberInArray(cpuNumber, cpuRandomNumber) == false) {
+        cpuNumber.push(cpuRandomNumber);
+    }
+    else {
+        i -= 1;
+    }
 }
 console.log('CPU: ' + cpuNumber);
 
@@ -9,14 +15,20 @@ var userNumberArray = [];
 var userNumber = parseInt(prompt('[' + counter + '] Inserisci un numero:'))
 
 while (counter !== 85 && checkNumberInArray(cpuNumber, userNumber) == false) {
-    counter += 1;
     console.log('Contatore: ' + counter);
     console.log('USER: ' + userNumberArray);
     userNumber = parseInt(prompt('[' + counter + '] Inserisci un numero:'))
-    userNumberArray.push(userNumber);
+    if (checkNumberInArray(userNumberArray, userNumber)) {
+        alert('Numero già inserito!')
+        counter -= 1;
+    }
+    else {
+        userNumberArray.push(userNumber);
+    }
     if (counter === 84) {
         alert('Hai vinto!\nI tuoi numeri: ' + userNumberArray + '\nNumeri CPU: ' + cpuNumber);
     }
+    counter += 1;
 }
 
 if (checkNumberInArray(cpuNumber, userNumber)) {
